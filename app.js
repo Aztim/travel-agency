9e85accdbfd816223d2671e176c10774a1d66481
Tim-82 <barboza-82@mail.ru>
2020-08-08 23:31:12 +0300

    add search section

--- app.js
+++ app.js
@@ -10,3 +10,18 @@ M.Slider.init(slider, {
     transition: 500,
     interval: 6000
   });
+
+// Auto Complete
+const ac = document.querySelector('.autocomplete');
+M.Autocomplete.init(ac, {
+    data: {
+      "Aruba": null,
+      "Cancun Mexico": null,
+      "Hawaii": null,
+      "Florida": null,
+      "California": null,
+      "Jamaica": null,
+      "Europe": null,
+      "The Bahamas": null,
+    }
+  });
